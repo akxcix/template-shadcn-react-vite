b2deab345742488ecbe1a40d67c1e592446a3439
akxcix <git@akxcix.com>
2024-05-27 03:10:47 -0400

    add vertical padding in layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -6,15 +6,14 @@ export default function Root() {
     return (
         <div id="root">
             <div className="flex flex-col h-screen">
-            <Navbar />
-            <Outlet />
-
-            <div className="grow">
-
+                <Navbar />
+                <div className="py-6">
+                    <Outlet />
+                </div>
+                <div className="grow"/>
+                <Footer />
             </div>
-            <Footer />
-            </div>
-            
+
         </div>
     );
 }
